e679bf5a83775f7340cf5b5403ac33711b85aade
Arsalan Khroush <arsalan_khroush@yahoo.com>
2022-11-11 14:10:45 +0300

    odev

---
+++ 4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+    //TS: toplam satir
+    //S: Satir
+    //B: bosluk
+    //SE: sembol
+    int TS, S, B, SE;
+    printf("satir sayiniz giriniz: ");
+    scanf("%d", &TS);
+    for(S=1; S<=TS; S++){
+            for(B=1; B<=(TS-S); B++){
+            printf(" ");}
+
+            for(SE=1; SE<=(2*S-1); SE++){
+            printf("*");}
+
+        printf("\n");
+
+    }
+}
